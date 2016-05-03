f01900d2ca3321140adb3029816e630535083210
Tim St. Clair <stclair@google.com>
2016-05-02 18:54:36 -0700

    Fixes for --disable_metrics flag
    1. Fix for writing to nil map when value is non-empty.
    2. Fix the usage string: go flag package uses backticks for the flag
    name.
    3. Render the flag default value in the same format as the flag accepts.

--- cadvisor.go
+++ cadvisor.go
@@ -72,12 +72,16 @@ type metricSetValue struct {
 }
 
 func (ml *metricSetValue) String() string {
-	return fmt.Sprint(*ml)
+	var values []string
+	for metric, _ := range ml.MetricSet {
+		values = append(values, string(metric))
+	}
+	return strings.Join(values, ",")
 }
 
 func (ml *metricSetValue) Set(value string) error {
-	ignoreMetrics = metricSetValue{}
 	if value == "" {
+		ml.MetricSet = nil
 		return nil
 	}
 	for _, metric := range strings.Split(value, ",") {
@@ -91,7 +95,7 @@ func (ml *metricSetValue) Set(value string) error {
 }
 
 func init() {
-	flag.Var(&ignoreMetrics, "disable_metrics", "comma-separated list of metrics to be disabled. Options are `disk`, `network`, `tcp`. Note: tcp is disabled by default due to high CPU usage.")
+	flag.Var(&ignoreMetrics, "disable_metrics", "comma-separated list of `metrics` to be disabled. Options are 'disk', 'network', 'tcp'. Note: tcp is disabled by default due to high CPU usage.")
 }
 
 func main() {
